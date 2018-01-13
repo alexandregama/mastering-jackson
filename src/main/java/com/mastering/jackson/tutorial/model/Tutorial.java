fd3716267863ca56303fef445ad3f03ce1c62517
Alexandre Gama <alexandre.gama.lima@gmail.com>
2018-01-13 16:54:10 -0200

    Adds the first draft

---
+++ src/main/java/com/mastering/jackson/tutorial/model/Tutorial.java
@@ -0,0 +1,43 @@
+package com.mastering.jackson.tutorial.model;
+
+public class Tutorial {
+
+	private Long id;
+	
+	private String title;
+	
+	private String language;
+	
+	public Tutorial(Long id, String title, String language) {
+		this.id = id;
+		this.title = title;
+		this.language = language;
+	}
+
+	public Tutorial() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUsedLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+}
--- src/test/java/com/mastering/jackson/tutorial/one/SimpleSerializationTest.java
+++ src/test/java/com/mastering/jackson/tutorial/one/SimpleSerializationTest.java
@@ -1,16 +1,18 @@
-## Configuring with Maven
+package com.mastering.jackson.tutorial.one;
 
-```xml 
-<dependency>
-  <groupId>com.fasterxml.jackson.core</groupId>
-  <artifactId>jackson-core</artifactId>
-  <version>${jackson-core-version}</version>
-</dependency>
-```
+import static org.junit.Assert.*;
 
-## Using a Simple Serialization with Jackson
+import org.junit.Test;
 
-```java
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.mastering.jackson.tutorial.model.Framework;
+import com.mastering.jackson.tutorial.model.Language;
+import com.mastering.jackson.tutorial.model.Tutorial;
+
+public class SimpleSerializationTest {
+
 	@Test
 	public void shouldSerializeInASimpleWay() throws Exception {
 		Tutorial tutorial = new Tutorial(1L, "CDI - How to use Decorators", "Java");
@@ -22,11 +24,7 @@
 		
 		assertEquals("{\"id\":1,\"title\":\"CDI - How to use Decorators\",\"language\":\"Java\"}", json);
 	}
-```
-
-## Printing a Pretty JSON with Jackson
-
-```java
+	
 	@Test
 	public void shouldSerializeWithAPrettyJsonBeingCreated() throws Exception {
 		Tutorial tutorial = new Tutorial(1L, "CDI - How to use Decorators", "Java");
@@ -36,17 +34,7 @@
 		
 		System.out.println(prettyJson);
 	}
-```
-
-## Printing a Pretty JSON at Runtime
-
-Just need to use:
-
-```java
-mapper.enable(SerializationFeature.INDENT_OUTPUT);
-```
-
-```java
+	
 	@Test
 	public void shouldPrintAPrettyJsonAtRuntime() throws Exception {
 		boolean shouldPrintPretty = true;
@@ -60,11 +48,7 @@ mapper.enable(SerializationFeature.INDENT_OUTPUT);
 		
 		System.out.println(prettyJson);
 	}
-```
-
-## Serializing Composed Java Objects with Jackson
-
-```java
+	
 	@Test
 	public void shouldSerializeComposeObjectsWithJackson() throws Exception {
 		Language java = new Language(1, "Java");
@@ -75,38 +59,7 @@ mapper.enable(SerializationFeature.INDENT_OUTPUT);
 		
 		System.out.println(prettyJson);
 	}
-```
-
-The output:
-
-```json
-{
-  "id" : 1,
-  "name" : "Spring",
-  "language" : {
-    "id" : 1,
-    "name" : "Java"
-  }
-}
-```
-
-## Serializing Getter Methods to JSON 
-
-Let's change the Tutorial class and rename the getter method getLanguage()
-
-```java
-public class Tutorial {
-
-	public String getUsedLanguage() {
-		return language;
-	}
 	
-}
-```
-
-Let's print again:
-
-```java
 	@Test
 	public void shouldSerializeGetterMethodsByDefault() throws Exception {
 		Tutorial tutorial = new Tutorial(1L, "CDI - How to use Decorators", "Java");
@@ -116,14 +69,5 @@ Let's print again:
 		
 		System.out.println(prettyJson);
 	}
-```
-
-The output:
-
-```json
-{
-  "id" : 1,
-  "title" : "CDI - How to use Decorators",
-  "usedLanguage" : "Java"
+	
 }
-```
